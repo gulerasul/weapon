b84def86d6a257280c38963140cc6e70b717410b
Guler Resulova <tu699evkp@aztu.local>
2022-03-15 16:57:04 +0400

    first commit

---
+++ ConsoleApp1/Models/Weapon.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Models
+{
+    class Weapon
+    {
+        public int bulletCapacityOfTheComb; //Darağın güllə tutumu
+        public int numberOfBulletsIntheComb; //Daraqdakı güllələrin sayı
+        public int secondsOfCombDischarge; //Boşalma saniyələri,
+        public int shootingMode; // atış modu
+
+        public Weapon(int bulletCapacityOfTheComb, int numberOfBulletsIntheComb, int secondsOfCombDischarge, int shootingMode)
+        {
+            this.bulletCapacityOfTheComb = bulletCapacityOfTheComb;
+            this.numberOfBulletsIntheComb = numberOfBulletsIntheComb;
+            this.secondsOfCombDischarge = secondsOfCombDischarge;
+            this.shootingMode = shootingMode;
+
+        }
+        public void Shoot()
+        {
+           
+            if (bulletCapacityOfTheComb>0 && numberOfBulletsIntheComb>0)
+            {
+                numberOfBulletsIntheComb = numberOfBulletsIntheComb - 1;
+
+
+                Console.WriteLine($"{"pew ,atesh achildi gulle sayi"} { numberOfBulletsIntheComb}");
+            }
+           
+             
+        }
+        public void Fire()
+        {
+            int endSeconds =  (numberOfBulletsIntheComb * secondsOfCombDischarge) / bulletCapacityOfTheComb;
+
+            Console.WriteLine(endSeconds);
+        }
+        public void GetRemainBulletCount()
+        {
+            int bulletNeededCount = bulletCapacityOfTheComb - numberOfBulletsIntheComb;
+
+            Console.WriteLine(bulletNeededCount);
+        }
+        public void Reload()
+        {
+            numberOfBulletsIntheComb = bulletCapacityOfTheComb;
+        }
+        public void ChangeFireMode()
+        {
+            if (shootingMode == 1)
+            {
+                shootingMode = 2;
+                Console.WriteLine("avtomatikdir");
+            }
+            else if (shootingMode == 2)
+            {
+                shootingMode = 1;
+                Console.WriteLine("teklidir");
+            }
+        }
+            
+    }
+    
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using ConsoleApp1.Models;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon gun = new Weapon(30, 12, 10, 5);
+
+             int key = 0;
+          
+            do
+            {
+                
+                Console.WriteLine("Reqem daxil edin : ");
+                key = Convert.ToInt32(Console.ReadLine());
+                switch (key)
+                {
+                    case 1:
+                        Console.WriteLine("1ci metod");
+                        gun.Shoot();
+                        break;
+                    case 2:
+                        Console.WriteLine("2ci metod");
+                        gun.Fire();
+                        Console.WriteLine($"{"butun gulleler ateshlendi "}" );
+                        break;
+                    case 3:
+                        Console.WriteLine("3cu metod");
+                        gun.GetRemainBulletCount();
+                        Console.WriteLine("daragin dolmasi uchun lazim olan gulle sayi");
+                        break;
+                    case 4:
+                        Console.WriteLine("4cu metod");
+                        gun.Reload();
+                        Console.WriteLine("daraq doldu");
+                        break;
+                    case 5:
+                        Console.WriteLine("5ci  metod");
+                        gun.ChangeFireMode();
+                        Console.WriteLine("atish mode deyishdi");
+                        break;
+                    case 6:
+                        Console.WriteLine("Sagolun");
+                        break;
+                    case 0:
+                        Console.WriteLine("0ci  metod");
+                        Console.WriteLine($"{"Daragin tutumu:"} {gun.bulletCapacityOfTheComb}");
+                        Console.WriteLine($"{"Daraqdaki gulle sayi:"} {gun.numberOfBulletsIntheComb}");
+                        Console.WriteLine($"{"Daragin bosalma saniyesi:"} {gun.secondsOfCombDischarge}");
+                        Console.WriteLine($"{"Atis modu:"} {gun.shootingMode}");
+                        break;
+                    default:
+                        Console.WriteLine("Sehv daxil etdiniz");
+                        break;
+                }
+            } while (key != 6);
+        }
+    }
+}
